356e14c63faf59932d976b8ade7d7f3ab2d90d88
Ethan Rogers <ethan@skuid.com>
2017-05-30 07:09:50 -0400

    update force cli version

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,7 +3,7 @@
 install_path=/usr/local/bin
 
 echo "Downloading and installing the Force CLI (https://github.com/heroku/force)..."
-wget -q -O ${install_path}/force https://github.com/heroku/force/releases/download/v0.22.65/force_darwin_amd64
+wget -q -O ${install_path}/force https://github.com/heroku/force/releases/download/v0.22.72/force_darwin_amd64
 chmod +x ${install_path}/force
 echo "Downloading and installing the Skuid CLI (https://github.com/skuid/skuid)..."
 wget -q -O ${install_path}/skuid https://github.com/skuid/skuid/releases/download/0.1.0/skuid_darwin_amd64
